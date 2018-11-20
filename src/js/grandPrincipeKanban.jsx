70277308e92681dcefd1bc823d2cad06ab6a1ddf
Djo69 <jlebrun@edenschool.fr>
2018-11-20 17:46:50 +0100

    Les grands principe 1

---
+++ src/js/grandPrincipeKanban.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class GrandPrincipe extends Component {
+  render() {
+    return(
+      <div className="LesGrandPrincipesKanban">
+        <h2>Les grands principes de la méthode kanban</h2>
+          <ul>
+            <li>
+              <i class="fas fa-thumbs-up"></i>
+            </li>
+
+            <li>
+              <i class="fas fa-hands-helping"></i>
+            </li>
+
+            <li>
+              <i class="fas fa-play"></i>
+            </li>
+
+            <li>
+              <i class="fas fa-undo-alt"></i>
+            </li>
+          </ul>
+      </div>
+    );
+  }
+}
+
+export default GrandPrincipe;
