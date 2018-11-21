28ab0265bf0b8a546d0fc0f9fd027f7c08261bae
Djo69 <jlebrun@edenschool.fr>
2018-11-21 15:57:53 +0100

    Grand Principe

--- src/js/grandPrincipeKanban.jsx
+++ src/js/grandPrincipeKanban.jsx
@@ -9,28 +9,40 @@ class GrandPrincipe extends Component {
             <li>
               <i className="fas fa-thumbs-up"></i>
               <div className="overlay">
-                <div className="text">Hello World</div>
+                <div className="text">
+                  <h3>Commencer pas finir</h3>
+                  <p>a méthode Kanban utilise les processus déjà en place et <br/>encourage une amélioration de ces processus.</p>
+                </div>
               </div>
             </li>
 
             <li>
               <i className="fas fa-hands-helping"></i>
               <div className="overlay">
-                <div className="text">Hello World</div>
+                <div className="text">
+                  <h3>Respectez le processus actuel, les rôles,
+                  les responsabilités et les titres</h3>
+                </div>
               </div>
             </li>
 
             <li>
               <i className="fas fa-play"></i>
               <div className="overlay">
-                <div className="text">Hello World</div>
+                <div className="text">
+                  <h3>Respectez le processus actuel, les rôles, les responsabilités et les titres</h3>
+                  <p>Afin de faciliter les changements à venir, il faut éliminer la crainte du changement, notamment en maintenant et en respectant les rôles, les responsabilités et les titres professionnels actuels de chacun.</p>
+                </div>
               </div>
             </li>
 
             <li>
               <i className="fas fa-undo-alt"></i>
               <div className="overlay">
-                <div className="text">Hello World</div>
+                <div className="text">
+                  <h3>Encourager le leadership</h3>
+                  <p>collaborateurs ou cadres supérieurs, tous les actes de leadership au sein de l’organisation doivent être encouragés.</p>
+                </div>
               </div>
             </li>
           </ul>
